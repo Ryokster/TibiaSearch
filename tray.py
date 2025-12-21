80e13dde7c5a2ae2908f2f5301623fcd174b12a0
Ryokster <wolfgangsommerbauer@gmail.com>
2025-12-21 11:21:49 +0100

    Add Tibia search desktop utility

---
+++ tray.py
@@ -0,0 +1,44 @@
+import threading
+from typing import Callable
+
+import pystray
+from PIL import Image, ImageDraw
+
+
+class TrayIcon:
+    def __init__(self, on_open: Callable[[], None], on_exit: Callable[[], None]) -> None:
+        self.on_open = on_open
+        self.on_exit = on_exit
+        self.icon = pystray.Icon("TibiaSearch", self._create_image(), "Tibia Search", self._create_menu())
+        self._is_running = False
+
+    def _create_image(self) -> Image.Image:
+        size = 64
+        image = Image.new("RGBA", (size, size), (30, 30, 30, 255))
+        draw = ImageDraw.Draw(image)
+        draw.rectangle((8, 8, size - 8, size - 8), fill=(173, 214, 255, 255))
+        draw.text((18, 18), "T", fill=(0, 0, 0, 255))
+        return image
+
+    def _create_menu(self) -> pystray.Menu:
+        return pystray.Menu(
+            pystray.MenuItem("Open", self._handle_open),
+            pystray.MenuItem("Exit", self._handle_exit),
+        )
+
+    def _handle_open(self, _icon: pystray.Icon, _item: pystray.MenuItem) -> None:
+        self.on_open()
+
+    def _handle_exit(self, _icon: pystray.Icon, _item: pystray.MenuItem) -> None:
+        self.on_exit()
+
+    def show(self) -> None:
+        if self._is_running:
+            return
+        self._is_running = True
+        threading.Thread(target=self.icon.run, daemon=True).start()
+
+    def stop(self) -> None:
+        if self._is_running:
+            self.icon.stop()
+            self._is_running = False
